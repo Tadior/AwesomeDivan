9419634caecc7ab854de40bc521fb46ecbb57b6b
Dmitriy <zamulin1998@gmail.com>
2023-07-25 16:47:25 +0300

    create footer

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,15 @@
-import { Header } from '@/widgets/Header'
-import { MainPage } from '@/pages/MainPage'
+import { Header } from '@/widgets/Header';
+import { MainPage } from '@/pages/MainPage';
+import { Footer } from '@/widgets/Footer';
 
 const App = () => {
   return (
     <>
       <Header />
       <MainPage />
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/widgets/Footer/ui/Footer.tsx
@@ -0,0 +1,26 @@
+import classNames from 'classnames';
+import cls from './Footer.module.css';
+
+export const Footer = () => {
+  return (
+    <footer className={classNames(cls.Footer)}>
+      <div className={classNames('container')}>
+        <div className={classNames(cls.Wrapper)}>
+          <div className={classNames(cls.Logo)}>
+            <a className={cls.Link} href="#">
+              <span className={cls.Span}>Divan</span>Awesome
+            </a>
+          </div>
+          <div className={classNames(cls.Info)}>
+            Вся информация и товары взяты с сайта{' '}
+            <a
+              className={classNames(cls.OriginalLink)}
+              href="https://divanboss.ru/">
+              Divan Boss
+            </a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
